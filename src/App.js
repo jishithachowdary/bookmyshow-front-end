e6f4cae47fd54f414f06da3822b11c2b55b522cb
jishithachowdary <nemanijc@gmail.com>
2021-07-27 20:28:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './stylesheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to Movie Booking Zone</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App';
+import './stylesheets/index.css';
+import { HallForm } from './HallForm';
+
+ReactDOM.render(<HallForm />, document.getElementById('root'));
